f09ac717709039b5b306a35353faeceaa4e170f1
SandarvaBaral <56055671+SandarvaBaral@users.noreply.github.com>
2019-10-03 21:45:03 +0100

    MegaByteConverter

---
+++ MegaBytesConverter.java
@@ -0,0 +1,26 @@
+public class MegaBytesConverter {
+    public static void main(String[] args) {
+
+        printMegaBytesAndKiloBytes(0);
+
+    }
+    /*
+        XX = Kilobytes
+        YY = megabytes
+        ZZ = remaining kilobytes %
+
+        XX KB = YY MB and ZZ KB
+
+        1 MB = 1024 kB
+     */
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        if(kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        }else if( kiloBytes >= 0) {
+            long result = kiloBytes / 1024;
+            long remainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + result + " MB and " + remainder + " KB" );
+        }
+    }
+}
